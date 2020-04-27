e7681f0f67a4309a8cf96ee4e4f5c42e7f51f29d
Govind Goel <52847415+govindgoel@users.noreply.github.com>
2020-04-27 14:24:34 +0530

    Update Double Cola.cpp

--- 82A/Double Cola.cpp
+++ 82A/Double Cola.cpp
@@ -1,1 +1,20 @@
+#include <stdio.h>
+#include <iostream>
 
+using namespace std;
+
+//Sheldon, Leonard, Penny, Rajesh and Howard
+int main(){
+    long long int n;
+    cin >> n;
+    int i = 1;
+    while (i * 5 < n){
+        n -= i * 5;
+        i *= 2;
+    }
+
+    string names[] = {"Sheldon", "Leonard", "Penny", "Rajesh", "Howard"};
+    cout << names[(n - 1) / i] << endl;
+
+    return 0;
+}
